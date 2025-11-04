61b70d0368a5d59a1eeaf0ffbfa34d9ae281025a
Robert <robertakurc@gmail.com>
2025-11-03 22:46:21 -0500

    Began work on adding translation support

--- scripts/autoload/game_manager.gd
+++ scripts/autoload/game_manager.gd
@@ -7,9 +7,9 @@ var game_states : Array[String] = ["Title", "Scene Transition", "Paused"]
 var scene : int = 0
 var scene_list : Array[PackedScene] = [preload("res://scenes/menu/main_menu.tscn")]
 
-@onready var pause_menu : PackedScene = preload("res://scenes/menu/pause_menu.tscn")
+var pause_menu : PackedScene = preload("res://scenes/menu/pause_menu.tscn")
 
-
+# Pausing Stuff
 func pause() -> void:
 	get_tree().paused = true
 	get_tree().current_scene.add_child(pause_menu.instantiate())
@@ -18,7 +18,7 @@ func unpause() -> void:
 	get_tree().paused = false
 	get_tree().current_scene.remove_child(get_tree().current_scene.find_child("Pause Menu"))
 
-
+# Scene Stuff
 func load_scene(new_scene : int = scene+1) -> void:
 	get_tree().paused = false
 	get_tree().change_scene_to_packed(scene_list[new_scene])
@@ -27,7 +27,7 @@ func load_scene(new_scene : int = scene+1) -> void:
 func get_scene() -> int:
 	return scene
 
-
+# Game State Stuff
 func set_game_state(new_state : String) -> void:
 	game_state = new_state
 
